4a2b998608d85580cdecb2383037ef780863b3e5
John Collins <j.m.collins@herts.ac.uk>
2014-12-19 21:32:54 +0000

    Correct missed mods

---
+++ fakeobs.py
@@ -0,0 +1,27 @@
+import re
+
+def getfakeobs(filename):
+    """Get contents of fake observation file as a dictionary.
+    Return None if invalid or cannot open"""
+    
+    try:
+        inf = open(filename, 'r')
+    except IOError:
+        return  None
+    
+    lm = re.compile('\s*(\S+)\s+([\d.]+)')
+    
+    red = dict()
+    
+    try:
+        for line in inf:
+            mtch = lm.match(line)
+            if not mtch: raise ValueError
+            fname = mtch.group(1)
+            ot = float(mtch.group(2))
+            red[fname] = ot
+        return  red
+    except ValueError:
+        return  None
+    finally:
+        inf.close()
