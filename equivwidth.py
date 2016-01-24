6539122107496b0138d57da182951a55ea3a2268
John M Collins <jmc@toad.me.uk>
2016-01-24 11:12:54 +0000

    Fixes to noise and equiv width.

--- equivwidth.py
+++ equivwidth.py
@@ -1,6 +1,7 @@
 # Equivalent width calc
 
 import scipy.integrate as si
+import numpy as np
 
 def equivalent_width(range, xvalues, yvalues, absorb = False):
     """Calculate equivalent width by integration.
@@ -15,3 +16,19 @@ def equivalent_width(range, xvalues, yvalues, absorb = False):
     if absorb:
         return -ret
     return  ret
+
+def equivalent_width_err(range, xvalues, yvalues, yerrs, absorb = False):
+    """Calculate equivalent width by integration and also error term.
+    
+    Assumes continuum normalised.
+
+    Args are range (datarange object) and lists of x values, yvalues and yerrs"""
+    
+    xvals, yvals, yes = range.select(xvalues, yvalues, yerrs)
+
+    wid = max(xvals) - min(xvals)
+    ret = si.trapz(yvals, xvals) - wid
+    rete = np.sqrt(np.sum(np.square(yes))) * wid 
+    if absorb:
+        return (-ret, rete)
+    return  (ret, rete)
--- noise.py
+++ noise.py
@@ -5,9 +5,12 @@ import numpy.random as nr
 import math
 
 def ms(s):
-    """Get root mean square of values"""
-
+    """Get mean square of values"""
     return np.mean(np.square(s))
+
+def rms(s):
+    """Get root mean square of values"""
+    return math.sqrt(np.mean(np.square(s)))
 
 def noise(sig, snr, unorm = 0.0):
     """Add given noise of given s/n ratio to given signal and return result"""
@@ -15,6 +18,27 @@ def noise(sig, snr, unorm = 0.0):
     nval = nr.uniform(-.5, .5, size = ls) * (1.0 - unorm) + nr.normal(size = ls) * unorm
     return sig + nval * np.sqrt(ms(sig) / (ms(nval) * 10.0 ** (snr / 10.0)))
 
+def rmsnoise(sig, snr, unorm = 0.0):
+    """Add given rms noise to given signal and return result"""
+    ls = len(sig)
+    nval = nr.uniform(-.5, .5, size = ls) * (1.0 - unorm) + nr.normal(size = ls) * unorm
+    return sig + nval * (rms(sig) / rms(nval)) / snr 
+
 def getnoise(sig, errs):
     """Calculate SNR from signal and errors"""
     return 10.0 * math.log10(ms(sig)/ms(errs))
+
+def getrmsnoise(sig, errs):
+    """Calculate RMS verions of SNR"""
+    return rms(sig)/rms(errs)
+
+def rms2db(rms):
+    """Convert rms SNR to dB copes with numpy vectors"""
+    return 20.0 * np.log10(rms)     # Times 2 to cancel sqrt and times 10 to dB
+
+def db2rms(db):
+    """Convert dB SNR to RMS coes with numpy vectors"""
+    try:    
+        return 10.0 ** (db / 20.0)
+    except TypeError:
+        return 10.0 ** (np.array(db) / 20.0)
