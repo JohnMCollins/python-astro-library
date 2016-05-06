4d9c8b17671a02830dc1df37c830b2f5c42e69fc
John M Collins <jmc@toad.me.uk>
2016-05-06 08:35:09 +0100

    Changes to allow percentages in histograms.

--- histandgauss.py
+++ histandgauss.py
@@ -5,13 +5,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as ss
 
-def histandgauss(values, bins=20, colour='blue', histalpha=0.8, plotalpha=1.0):
+def histandgauss(values, bins=20, colour='blue', histalpha=0.8, plotalpha=1.0, asperc=False):
     """Display histogram with given number of bins and colour and overplotted gaussian"""
 
     if isinstance(colour, str):
         colour = string.split(colour, ',')
 
-    freqs, sizes, patches = plt.hist(values, bins=bins, color=colour[0], alpha=histalpha)
+    if asperc:
+        scale = 100.0 / len(values)
+        freqs, sizes, patches = plt.hist(values, bins=bins, color=colour[0], alpha=histalpha, weights=np.ones_like(values) * scale)
+    else:
+        scale = 1.0
+        freqs, sizes, patches = plt.hist(values, bins=bins, color=colour[0], alpha=histalpha)
 
     resc = np.sum(freqs * np.diff(sizes))
 
