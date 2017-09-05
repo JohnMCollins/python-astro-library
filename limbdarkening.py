69b3f83a976903669174c9849b823100def0f016
John Collins <j.m.collins@herts.ac.uk>
2017-09-05 14:44:09 +0100

    Changes 05/09/17

--- limbdarkening.py
+++ limbdarkening.py
@@ -3,6 +3,25 @@
 import numpy as np
 import math
 
+def claret_linear(mu, coeff):
+    """Calculate Claret-sytle linear (nb scalar coeff)"""
+    return 1.0 - coeff * (1.0 - mu)
+
+def claret_quadratic(mu, coeffs):
+    """Calculcate Claret-style quadratic"""
+    a, b = coeffs
+    return 1.0 - a * (1.0 - mu) - b * (1.0 - mu)**2
+
+def claret_sqrt(mu, coeffs):
+    """Calculate Clater-style square root"""
+    c, d = coeffs
+    return 1.0 - c * (1.0 - mu) - d * (1.0 - math.sqrt(mu))
+
+def claret_log(mu, coeffs):
+    """Calculate Claret-style logarithmic"""
+    e, f = coeffs
+    return 1.0 - e * (1.0 - mu) - f * mu * math.log(mu)
+
 def claret_nonlin(mu, coeffs):
     """Calculate Claret-style non-linear"""
     smu = math.sqrt(mu)
