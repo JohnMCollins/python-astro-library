e954af2f3db2021287d229a04febef9c3139ca76
John M Collins <jmc@toad.me.uk>
2017-10-23 09:09:03 +0100

    Update credentials and operations.

--- dbcredentials.py
+++ dbcredentials.py
@@ -2,13 +2,90 @@
 
 import ConfigParser
 import os.path
+import os
+
+configfilepaths = ("/etc/dbcred.ini", os.path.expanduser("~/lib/dbcred.ini"), os.path.abspath('.dbcred.ini'))
+selectconfig = dict(current = 2, lib = 1, system = 0)
+vardict = dict(DBHOST = 'localhost')
+for v in ('LOGNAME', 'DBHOST'):
+    try:
+        vardict[v] = os.environ[v]
+    except KeyError:
+        pass
+
+class DBcredError(Exception):
+    pass
 
 class DBcred(object):
+    """This class uses ConfigParse to read and optionally write a "ini" type file
+    of database credentials.
+    This can be ".dbcred.ini" in the current directory,
+    ~/lib/dbcred.ini in "lib" of the user's home directory or
+    /etc/dbcredi.ini
+    We work in terms of tuples (host, dbname, user, password)""" 
     
     def __init__(self):
+        global configfilepaths
         self.cparser = ConfigParser.SafeConfigParser()
-        self.cparser.read(("/etc/dbcred.ini", os.path.expanduser("~/lib/dbcred.ini"), '.dbcred.ini'))
+        self.cparser.read(configfilepaths)
 
     def get(self, name):
         """Get credentials for the name"""
+        try:
+            return  (self.cparser.get(name, 'host', 0, vardict),
+                     self.cparser.get(name, 'database', 0, vardict),
+                     self.cparser.get(name, 'user', 0, vardict),
+                     self.cparser.get(name, "password", 1))
         
+        except ConfigParser.NoSectionError:
+            raise DBcredError("Section " + name + " not known")
+        except ConfigParser.NoOptionError:
+            raise DBcredError("Section " + name + " not fully defined")
+    
+    def set_defaults(self, creds):
+        """Set defaults according to list. 
+        Items in list are new value or False to leave unchanged NOne to remove"""
+              
+        for optn, val in zip(('host', 'database', 'user', 'password'), creds):
+            if  val is None:
+                self.cparser.remove_option('DEFAULT', optn)
+            elif val:
+                self.cparser.set('DEFAULT', optn, val)
+    
+    def set_creds(self, name, creds):
+        """Set crdds for named according to list. 
+        Items in list are new value or False to leave unchanged NOne to remove"""
+        
+        if not self.cparser.has_section(name):
+            self.cparser.add_section(name)
+        
+        for optn, val in zip(('host', 'database', 'user', 'password'), creds):
+            if  val is None:
+                self.cparser.remove_option(name, optn)
+            elif val:
+                self.cparser.set(name, optn, val)
+    
+    def delcreds(self, name):
+        """Delete credentials for given DB"""
+        self.cparser.remove_section(name)
+    
+    
+    def write(self, filename = "lib"):
+        """Write credentials to filename.
+        Special cases of filename are "current", "lib" and "sys" to
+        write to standard names in current directory, ~/lib and system
+        respectively"""
+        
+        global configfilepaths, selectconfig
+        
+        if filename in selectconfig:
+            filename = configfilepaths[selectconfig[filename]]
+        try:
+            outf = open(filename, "wb")
+        except IOError as e:
+            raise DBcredError("Cannoto open file " + filename + " - " + e.args[1])
+        
+        self.cparser.write(outf)
+        outf.close()
+
+    
---
+++ radec.py
@@ -0,0 +1,59 @@
+# Right ascension and declination conversions
+
+import string
+import math
+
+class RADconvError(Exception):
+    pass
+
+def ra_hr2deg(ra):
+    """Convert RA in hours (possibly string) to degrees"""
+    try:
+        return ra * 15.0
+    except TypeError:
+        bits = string.split(ra, ':')
+        if len(bits) != 3:
+            raise RADconvError("Expecting 3-part RA string")
+        try:
+            return  reduce(lambda a, b: a*60.0 + b, [float(c) for c in bits]) / 240.0
+        except TypeError:
+            raise RADconvError("Invalid RA string " + ra)
+        
+
+def ra_deg2hr(ra, asstr = False):
+    """Convert RA in degrees to hours or as string if asstr is true"""
+    if asstr:
+        ra /= 15.0
+        h = int(math.floor(ra))
+        ra = (ra - h) * 60
+        m = int(math.floor(ra))
+        ra = (ra - m) * 60
+        return  "%.2d:%.2d:" % (h, m) + ("%.2f" % (ra + 100.))[1:]
+    return ra / 15.0
+
+
+def dec2float(dec):
+    """Convert declination string to float"""
+    sig = 1.0
+    if dec[0] == '-':
+        dec = dec[1:]
+        sig = -1.0
+    bits = string.split(dec, ':')
+    if len(bits) != 3:
+            raise RADconvError("Expecting 3-part DEC string")
+    try:
+        return math.copysign(reduce(lambda a, b: a*60.0 + b, [float(c) for c in bits]), sig) / 3600.0
+    except TypeError:
+        raise RADconvError("Invalid DEC string " + dec)
+
+def dec2str(dec):
+    """Convert declination flat to string"""
+    sig = ""
+    if dec < 0.0:
+        dec = - dec
+        sig = "-"
+    d = int(math.floor(dec))
+    dec = (dec - d) * 60
+    m = int(math.floor(dec))
+    dec = (dec - m) * 60
+    return  sig + "%.2d:%.2d:" % (d, m) + ("%.2f" % (dec + 100.))[1:]
