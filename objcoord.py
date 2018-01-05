c91c8d25af8f7dc71e2ed68e441e8572318e08fd
John Collins <j.m.collins@herts.ac.uk>
2018-01-05 14:42:49 +0000

    Changes 05/01/18

--- objcoord.py
+++ objcoord.py
@@ -4,7 +4,7 @@ from astroquery.simbad import Simbad
 from astropy import coordinates
 import astropy.units as u
 
-def coord2obs(ra, dec, radius=2, objtype = None):
+def coord2objs(ra, dec, radius=2, objtype = None):
     """Get objects within given radius of given ra/dec as decimal degrees.
     If object type given, restrict to that"""
     
@@ -13,21 +13,21 @@ def coord2obs(ra, dec, radius=2, objtype = None):
     sb = Simbad()
     sb.add_votable_fields('otype')
     sres = sb.query_region(sk, radius=rad)
-    ids = sres['MAIN_ID'][:]
+    if sres is None: return []
+    ids = [id for id in sres['MAIN_ID']]
     if objtype is None:
         return ids
-    otypes = sres['OTYPE'][:]
+    otypes = [ot for ot in sres['OTYPE']]
     resids = []
     for ob,nxt in zip(otypes,ids):
         if ob in objtype:
             resids.append(nxt)
     return resids
 
-def obs2coord(obj):
+def obj2coord(obj):
     """Get coordinates of object as (ra, dec) decimals"""
     
     sb = Simbad().query_object(obj)
     if sb is None: return None
     sk = coordinates.SkyCoord(ra=sb['RA'][0], dec=sb['DEC'][0], unit=(u.hour,u.deg))
     return (sk.ra.deg, sk.dec.deg)
-     
