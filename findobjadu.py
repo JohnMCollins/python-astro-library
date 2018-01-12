90cee83c10faa628903d4e44e1cb4a00b5c7cc14
John Collins <j.m.collins@herts.ac.uk>
2018-01-12 14:28:52 +0000

    Changes 12/01/18

--- findobjadu.py
+++ findobjadu.py
@@ -13,10 +13,18 @@ def findobjadu(w, imagedata, radec, searchwidth, apwidth = 6):
         searchwidth - pixels we search either way
         apwidth (default 6) aperture dith - using circular aperture
         
+        Given ValueError if aperture > searchwidth
+        
     Returns:
     
         None if it cannot find it
         tuple (pixx, pixy, adu)"""
+    
+    if apwidth > searchwidth:
+        raise ValueError("findobjadvu: aperture > searchwidth")
+    
+    # Create matrix the same shape as imagedata with circular aperture radius apwidth centred
+    # on the initial coords
     
     pixrows, pixcols = imagedata.shape
     cornerpix = np.array(((0,0), (pixcols-1, 0), (0, pixrows-1), (pixcols-1, pixrows-1)), np.float)
@@ -24,9 +32,10 @@ def findobjadu(w, imagedata, radec, searchwidth, apwidth = 6):
     ramax, decmax = cornerradec.max(axis=0)
     ramin, decmin = cornerradec.min(axis=0)
     raobj, decobj = radec
-    if raobj <= ramin or raobj >= ramax: return 2
-    if decobj <= decmin or decobj >= decmax: return 2
     objx, objy = w.wcs_world2pix(np.array([radec]), 0).flatten()
-    if objx <= searchwidth or objx >= searchwidth + pixcols: return 1
-    if objy <= searchwidth or objy >= searchwidth + pixcols: return 1
+    
+    rads = np.sqrt(np.add.outer((np.arange(0,pixrows)-objy)**2,(np.arange(0,pixcols)-objx)**2))
+    mask = np.zeros_like(imagedata)
+    mask[rads <= apwidth] = 1.0
+    
     return 0
