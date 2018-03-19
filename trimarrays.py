0720346e102addacf0d00ed6d9d4c3c0063483c6
John M Collins <jmc@toad.me.uk>
2018-03-19 23:47:26 +0000

    Add array trim stuff

---
+++ trimarrays.py
@@ -0,0 +1,34 @@
+# Trim Nan values from edges of array
+
+import numpy as np
+
+def trimnan(arr):
+    """Trim NaN values from last rows and columns of 2D array.
+    
+    Arg is orignal array,
+    Rturn trimmed array"""
+    
+    rows, cols = arr.shape
+    while np.count_nonzero(np.isnan(arr[-1])) == cols:
+        arr = arr[0:-1]
+        rows -= 1
+    while np.count_nonzero(np.isnan(arr[:,-1])) != 0:
+        arr = arr[:,0:-1]
+    while np.count_nonzero(np.isnan(arr[-1])) != 0:
+        arr = arr[0:-1]
+    return  arr
+
+def trimto(arr, *arrs):
+  
+    """Trim arrs to size of first array"""
+   
+    rows, cols = arr.shape
+    
+    result = []
+    
+    for a in arrs:
+        result.append(a[0:rows, 0:cols])
+    
+    return tuple(result)
+
+    
