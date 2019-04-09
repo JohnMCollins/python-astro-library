fc3efee3d2259035abf1e1b9f5c308adec5a5aaa
John M Collins <jmc@toad.me.uk>
2019-04-09 08:47:49 +0100

    Add remdefaults.

---
+++ remdefaults.py
@@ -0,0 +1,26 @@
+# Save options in home control file for later recovery
+
+import os
+import sys
+
+def default_database():
+    """Get default database depending on what host we are on"""
+    
+    hostn = os.uname().nodename
+    if hostn == "nacny" or hostn == "foxy" :
+        return "remfits"
+    if hostn == "uhhpc":
+        return "cluster"
+    try:
+        hostn.index("herts.ac.uk")
+        return "cluster"
+    except ValueError:
+        pass
+    return "remfits"
+
+def get_tmpdir():
+    """Select an appropriate temporty directory"""
+    try:
+        return  os.environ["REMTMP"]
+    except KeyError:
+        return  os.getcwd()
