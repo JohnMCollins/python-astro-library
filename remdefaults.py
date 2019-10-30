935f062a919072f8a2d4965caf792fee75a22b3a
John M Collins <jmc@toad.me.uk>
2019-10-30 10:19:53 +0000

    Fix back temp fix and add in envir check.

--- remdefaults.py
+++ remdefaults.py
@@ -6,8 +6,12 @@ import sys
 def default_database():
     """Get default database depending on what host we are on"""
     
+    try:
+        return os.environ["REMDB"]
+    except KeyError:
+        pass;
     hostn = os.uname().nodename
-    if hostn == "nancy" or hostn == "foxy" :
+    if hostn == "lexi" or hostn == "nancy" or hostn == "foxy" :
         return "remfits"
     if hostn == "uhhpc":
         return "cluster"
@@ -16,7 +20,7 @@ def default_database():
         return "cluster"
     except ValueError:
         pass
-    return "cluster"            # Temporary whilst foxy and nacncy are offline
+    return "remfits"
 
 def get_tmpdir():
     """Select an appropriate temporty directory"""
